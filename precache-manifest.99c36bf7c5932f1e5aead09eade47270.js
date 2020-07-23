5116a02c0a25e29d7057e2e68e97c6a310f091d7
Andrew Fryer <andrew_fryer@hotmail.co.uk>
2020-07-23 09:20:42 +0100

    Updates

---
+++ precache-manifest.99c36bf7c5932f1e5aead09eade47270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268c4a37823f24dc2e2ac8b5b4e0c377",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "7602d5b70c49f39dc4e1",
+    "url": "/monstersrolodex/static/css/main.922753c7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstersrolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7602d5b70c49f39dc4e1",
+    "url": "/monstersrolodex/static/js/main.a372bec1.chunk.js"
+  },
+  {
+    "revision": "96f0135039460d4bf74f",
+    "url": "/monstersrolodex/static/js/runtime-main.4a497939.js"
+  }
+]);
