f16f563bb66ced9808528e6869748546b5801cac
Andrew Fryer <andrew_fryer@hotmail.co.uk>
2020-07-23 10:59:55 +0100

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
+import andrew from './images/andrew.jpg';
+import sarah from './images/sarah.jpg';
+import obie from './images/obie.jpg';
+
 import './App.css';
 
 class App extends Component {
@@ -15,9 +19,32 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(response => response.json())
+    // .then(users => this.setState({ monsters: users }));
+
+    this.setState({
+      monsters: [
+        {
+          id: 1,
+          name: 'Andrew the awesome',
+          email: 'andrew@andrew.com',
+          image: andrew
+        },
+        {
+          id: 2,
+          name: 'Sarah the sultry',
+          email: 'sarah@sarah.com',
+          image: sarah
+        },
+        {
+          id: 3,
+          name: 'Obie the magnificent',
+          email: 'obie@obie.com',
+          image: obie
+        },
+      ]
+    })
   }
 
   handleChange = (e) => {
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className="card-container"> 
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+        <img id={props.monster.name} alt="monster" src={props.monster.image} width="300px" height="400px" />
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email} </p>
     </div>
